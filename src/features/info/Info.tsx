fca6fea2e925ef4a98d3aca4997e98a2822dbe49
Igor Storchevoy <hellsgor@gmail.com>
2025-02-26 22:39:01 +0400

    Feat[info]: create component

---
+++ src/features/info/Info.tsx
@@ -0,0 +1,21 @@
+import classes from './Info.module.css';
+import { InfoData } from '../../models';
+import { useAppSelector } from '../../app/hooks';
+
+const Info = () => {
+  const data: InfoData = useAppSelector((state) => state.info);
+
+  return (
+    <div className={classes.info}>
+      <span>Player</span>
+      <span
+        className={`${classes.player} ${
+          data.currentPlayer === 'o' ? classes.o : classes.x
+        }`}
+      ></span>
+      <span>move #{data.move}</span>
+    </div>
+  );
+};
+
+export default Info;
