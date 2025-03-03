3601d530e37fb381eb462b9b161f3f525766980b
Igor Storchevoy <hellsgor@gmail.com>
2025-03-03 14:28:09 +0400

    Feat[useWinChecker]: created hook

---
+++ src/hooks/useWinChecker.ts
@@ -0,0 +1,27 @@
+import { CellValue } from '../models';
+
+export const useWinChecker = (cells: CellValue[]) => {
+  const winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const win = winCombinations.filter((combination) => {
+    const reference: CellValue = cells[combination[0]];
+    if (!reference) return;
+
+    return combination.every((combinationCell) => {
+      return !!cells[combinationCell] && cells[combinationCell] === reference;
+    });
+  });
+
+  console.log('win', win);
+
+  return win;
+};
