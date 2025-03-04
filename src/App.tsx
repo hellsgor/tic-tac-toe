4f4b20ae4e9f8bec292dcac71b3420952d321829
Igor Storchevoy <hellsgor@gmail.com>
2025-03-04 07:51:28 +0400

    Update[App]: used useWinChecker

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css';
 import { Field } from './features/field';
 import { Info } from './features/info';
+import { useWinChecker } from './hooks/useWinChecker';
 
 function App() {
+  const win = useWinChecker();
+  console.log(win);
   return (
     <>
       <h1>Tic Tac Toe</h1>
