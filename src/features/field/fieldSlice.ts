0711a0ca4114b25d009265fe4b1f91c9886dc28d
Igor Storchevoy <hellsgor@gmail.com>
2025-03-07 22:58:14 +0400

    Feat[fieldSlice]: created resetField reducer

--- src/features/field/fieldSlice.ts
+++ src/features/field/fieldSlice.ts
@@ -12,8 +12,12 @@ export const fieldSlice = createSlice({
     setSymbol: (state, action: PayloadAction<CellPayload>) => {
       state.value[action.payload.index] = action.payload.symbol;
     },
+
+    resetField: (state) => {
+      state.value = initialState.value;
+    },
   },
 });
 
-export const { setSymbol } = fieldSlice.actions;
+export const { setSymbol, resetField } = fieldSlice.actions;
 export default fieldSlice.reducer;
