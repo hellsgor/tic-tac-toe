3483a7acb19c4a9a97c488ee10b6711b00de7bfb
Igor Storchevoy <hellsgor@gmail.com>
2025-03-16 16:11:04 +0400

    Feat[store]: created store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import StoreProvider from "./StoreProvider";
 import { Field } from "@/components/Field/Field";
 
 export default function Home() {
   return (
-    <>
+    <StoreProvider>
       <h1 className="text-6xl font-bold">Tic Tac Toe</h1>
       <Field />
-    </>
+    </StoreProvider>
   );
 }
