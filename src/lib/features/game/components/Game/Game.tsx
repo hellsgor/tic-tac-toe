f3572305db43b95f843caffaf28e4489f012eb8b
Igor Storchevoy <hellsgor@gmail.com>
2025-03-16 23:54:14 +0400

    Feat[Game]: used useGameChecker & Info

--- src/lib/features/game/components/Game/Game.tsx
+++ src/lib/features/game/components/Game/Game.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { useGameChecker } from "../../hooks/useGameChecker";
 import Field from "../Field/Field";
 import Info from "../Info/Info";
 
 export function Game() {
+  useGameChecker();
+
   return (
-    <>
+    <div className="relative">
       <Field />
-    </>
+      <Info classes="absolute top-0 left-full pl-10 h-full" />
+    </div>
   );
 }
