a0c54c3ab45819104672b465999695870209b41a
Igor Storchevoy <hellsgor@gmail.com>
2025-03-16 16:16:08 +0400

    Feat[game]: created game slice

--- src/lib/reducers.ts
+++ src/lib/reducers.ts
@@ -1,5 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import gameReducer from "./features/game/game";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  game: gameReducer,
+});
 
 export default rootReducer;
