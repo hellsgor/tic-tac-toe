0d12ae9c16a574dbae140be8ed04215aaf03ffb9
Igor Storchevoy <hellsgor@gmail.com>
2025-03-17 10:48:49 +0400

    Fix[Cell]: refactored win class assignment to use cell value instead of current player

--- src/lib/features/game/components/Cell/Cell.tsx
+++ src/lib/features/game/components/Cell/Cell.tsx
@@ -1,17 +1,15 @@
-import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { useAppDispatch } from "@/lib/hooks";
 import { CellProps, CellValue } from "@/models";
 import { memo } from "react";
 import { incrementMove, setCurrentPlayer, setSymbol } from "../../game";
 import { getString } from "@/helpers/getString";
 
 const Cell = ({ value, index, win }: CellProps) => {
-  const currentPlayer = useAppSelector((state) => state.game.currentPlayer);
-
   const getClasses = (value: CellValue) =>
     getString(
       [
         "border-none",
-        `${win ? (currentPlayer === "x" ? "bg-(--color-o-win-bg)" : "bg-(--color-x-win-bg)") : "bg-[#242424]"}`,
+        `${win ? (value === "x" ? "bg-(--color-o-win-bg)" : "bg-(--color-x-win-bg)") : "bg-[#242424]"}`,
       ],
       value
         ? [
