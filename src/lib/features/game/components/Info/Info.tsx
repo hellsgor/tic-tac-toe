3b7348cb93facec51189237c6f9c0ca60f6d99de
Igor Storchevoy <hellsgor@gmail.com>
2025-03-18 10:13:34 +0400

    Refactor[Info]: move getStatus function inside Info component and update its signature

--- src/lib/features/game/components/Info/Info.tsx
+++ src/lib/features/game/components/Info/Info.tsx
@@ -2,28 +2,15 @@ import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { InfoProps } from "@/models";
 import { getString } from "@/helpers/getString";
 import { Button } from "@/UI/Button/Button";
-import { reset } from "../../game";
+import { GameState, reset } from "../../game";
 
-const Info = ({ classes, win, draw }: InfoProps) => {
+export default function Info({ classes, win, draw }: InfoProps) {
   const rowClasses = "flex items-center gap-x-1 text-base";
 
   const move = useAppSelector((state) => state.game.move);
   const currentPlayer = useAppSelector((state) => state.game.currentPlayer);
 
   const dispatch = useAppDispatch();
-
-  const getStatus = () =>
-    draw ? (
-      <span>The game ended with a draw</span>
-    ) : (
-      <>
-        <span>Player</span>
-        <span
-          className={`inline-block size-6 bg-[url(/icons/${win.length ? (currentPlayer === "x" ? "o" : "x") : currentPlayer}.svg)] bg-contain bg-center bg-no-repeat`}
-        ></span>
-        <span>{win.length ? " won!" : " move"}</span>
-      </>
-    );
 
   const handleResetBtnClick = () => {
     dispatch(reset());
@@ -36,7 +23,7 @@ const Info = ({ classes, win, draw }: InfoProps) => {
         <p className={rowClasses}>
           <span>Move #{win || draw ? move - 1 : move}</span>
         </p>
-        <p className={rowClasses}>{getStatus()}</p>
+        <p className={rowClasses}>{getStatus({ win, draw, currentPlayer })}</p>
         {/* <p className={rowClasses}>
           <span>Next move in:</span>
         </p> */}
@@ -45,6 +32,20 @@ const Info = ({ classes, win, draw }: InfoProps) => {
       <Button onClick={handleResetBtnClick}>Start new game</Button>
     </div>
   );
-};
+}
 
-export default Info;
+type GetStatusProps = Omit<InfoProps, "classes"> &
+  Pick<GameState, "currentPlayer">;
+
+const getStatus = ({ win, draw, currentPlayer }: GetStatusProps) =>
+  draw ? (
+    <span>The game ended with a draw</span>
+  ) : (
+    <>
+      <span>Player</span>
+      <span
+        className={`inline-block size-6 bg-[url(/icons/${win.length ? (currentPlayer === "x" ? "o" : "x") : currentPlayer}.svg)] bg-contain bg-center bg-no-repeat`}
+      ></span>
+      <span>{win.length ? " won!" : " move"}</span>
+    </>
+  );
