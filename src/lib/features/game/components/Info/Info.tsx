899158b8a89021f6268b379871aec4fe71f31966
Igor Storchevoy <hellsgor@gmail.com>
2025-03-18 12:18:55 +0400

    Refactor[Info]: introduce InfoRow component

--- src/lib/features/game/components/Info/Info.tsx
+++ src/lib/features/game/components/Info/Info.tsx
@@ -4,10 +4,9 @@ import { getString } from "@/helpers/getString";
 import { Button } from "@/UI/Button/Button";
 import { reset } from "../../game";
 import { getStatus } from "./getStatus";
+import InfoRow from "./Row";
 
 export default function Info({ classes, win, draw }: InfoProps) {
-  const rowClasses = "flex items-center gap-x-1 text-base";
-
   const move = useAppSelector((state) => state.game.move);
   const currentPlayer = useAppSelector((state) => state.game.currentPlayer);
 
@@ -21,13 +20,10 @@ export default function Info({ classes, win, draw }: InfoProps) {
     <div className={getString(classes)}>
       <div className="bg-dark-bg flex min-w-xs flex-auto flex-col gap-y-1 rounded-lg p-4">
         <h4 className="mb-4 text-xl font-bold">Info: </h4>
-        <p className={rowClasses}>
+        <InfoRow>
           <span>Move #{win || draw ? move - 1 : move}</span>
-        </p>
-        <p className={rowClasses}>{getStatus({ win, draw, currentPlayer })}</p>
-        {/* <p className={rowClasses}>
-          <span>Next move in:</span>
-        </p> */}
+        </InfoRow>
+        <InfoRow>{getStatus({ win, draw, currentPlayer })}</InfoRow>
       </div>
 
       <Button onClick={handleResetBtnClick}>Start new game</Button>
