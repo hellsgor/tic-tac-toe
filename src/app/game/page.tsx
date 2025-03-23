033506fc71f9995a7a5efa0c588284f8840c54f5
Igor Storchevoy <hellsgor@gmail.com>
2025-03-23 17:38:35 +0400

    Feat[Game]: wrap game components in Container

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -3,18 +3,21 @@
 import { useGameChecker } from "../../lib/features/game/hooks/useGameChecker";
 import Field from "../../lib/features/game/components/Field/Field";
 import Info from "../../lib/features/game/components/Info/Info";
+import { Container } from "@/UI/Container";
 
 export default function Game() {
   const { win, draw } = useGameChecker();
 
   return (
-    <div className="relative">
-      <Field win={win} />
-      <Info
-        win={win}
-        draw={draw}
-        classes="absolute top-0 left-full pl-10 h-full flex flex-col justify-between gap-y-5"
-      />
-    </div>
+    <Container>
+      <div className="flex w-full flex-col items-center justify-center gap-10">
+        <Info
+          win={win}
+          draw={draw}
+          classes="flex flex-col justify-between gap-y-5"
+        />
+        <Field win={win} />
+      </div>
+    </Container>
   );
 }
