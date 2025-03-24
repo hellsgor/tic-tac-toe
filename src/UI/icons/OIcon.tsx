9d80541b40435589a1f53b95a1f3bc581b57c889
Igor Storchevoy <hellsgor@gmail.com>
2025-03-25 00:44:32 +0400

    Update[Icons]: added size prop to OIcon and XIcon components

--- src/UI/icons/OIcon.tsx
+++ src/UI/icons/OIcon.tsx
@@ -1,11 +1,11 @@
 import { memo } from "react";
 
-const OIcon = memo(() => {
+const OIcon = memo(({ size }: { size?: string }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="100%"
-      height="100%"
+      width={size || "100%"}
+      height={size || "100%"}
       viewBox="0 0 24 24"
       fill="none"
       stroke="#9acd32"
