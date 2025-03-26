98ff39a284e188065ebe5ca9d8d034e8f5868acc
Igor Storchevoy <hellsgor@gmail.com>
2025-03-27 02:35:23 +0400

    Refactor[Field]: extract class name logic into classes constant

--- src/lib/features/game/components/Field/Field.tsx
+++ src/lib/features/game/components/Field/Field.tsx
@@ -6,14 +6,13 @@ import clsx from "clsx";
 export default function Field({ win }: FieldProps) {
   const cells = useAppSelector((state) => state.game.cells);
   const winIndices = new Set(win.length ? win[0] : []);
+  const classes = clsx(
+    "grid aspect-square w-100 grid-cols-3 grid-rows-3 gap-0.25 bg-[#434343]",
+    win.length ? "pointer-events-none" : null,
+  );
 
   return (
-    <div
-      className={clsx(
-        "grid aspect-square w-100 grid-cols-3 grid-rows-3 gap-0.25 bg-[#434343]",
-        win.length ? "pointer-events-none" : null,
-      )}
-    >
+    <div className={classes}>
       {cells.map((cell, idx) => (
         <Cell key={idx} value={cell} index={idx} win={winIndices.has(idx)} />
       ))}
