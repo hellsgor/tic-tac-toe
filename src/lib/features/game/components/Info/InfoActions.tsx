5a65ae29c286297a49f11a83c1bb9bd16cfa4d8f
Igor Storchevoy <hellsgor@gmail.com>
2025-04-27 23:55:44 +0400

    Feat[InfoActions]: add winner and draw state handling with dynamic opacity

--- src/lib/features/game/components/Info/InfoActions.tsx
+++ src/lib/features/game/components/Info/InfoActions.tsx
@@ -1,13 +1,21 @@
-import { useAppDispatch } from "@/lib/hooks";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { MemoizedButton } from "@/UI/Button/Button";
 import { memo } from "react";
 import { reset } from "../../game";
+import clsx from "clsx";
 
 export const InfoActions = memo(function InfoActions() {
   const dispatch = useAppDispatch();
+  const winner = useAppSelector((state) => state.game.winner);
+  const draw = useAppSelector((state) => state.game.draw);
 
   return (
-    <div className="flex items-center gap-x-5">
+    <div
+      className={clsx(
+        "flex items-center gap-x-5 transition-opacity",
+        `opacity-${winner || draw ? 1 : 0}`,
+      )}
+    >
       <MemoizedButton onClick={() => dispatch(reset())} className="w-full">
         Offer a draw
       </MemoizedButton>
