3c5af7dc1274033bf864a41aa2b5a5c35325295d
Igor Storchevoy <hellsgor@gmail.com>
2025-04-27 23:56:42 +0400

    Feat[InfoActions]: implement timer functionality and update button labels for game actions

--- src/lib/features/game/components/Info/InfoActions.tsx
+++ src/lib/features/game/components/Info/InfoActions.tsx
@@ -1,6 +1,6 @@
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { MemoizedButton } from "@/UI/Button/Button";
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
 import { reset } from "../../game";
 import clsx from "clsx";
 
@@ -8,6 +8,20 @@ export const InfoActions = memo(function InfoActions() {
   const dispatch = useAppDispatch();
   const winner = useAppSelector((state) => state.game.winner);
   const draw = useAppSelector((state) => state.game.draw);
+
+  const [timer, setTimer] = useState(10);
+
+  useEffect(() => {
+    if (!winner && !draw) {
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setTimer((prev) => Math.max(0, prev - 1));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [winner, draw]);
 
   return (
     <div
@@ -17,9 +31,9 @@ export const InfoActions = memo(function InfoActions() {
       )}
     >
       <MemoizedButton onClick={() => dispatch(reset())} className="w-full">
-        Offer a draw
+        Play again
       </MemoizedButton>
-      <MemoizedButton className="w-full">Surrender</MemoizedButton>
+      <MemoizedButton className="w-full">New game ({timer})</MemoizedButton>
     </div>
   );
 });
