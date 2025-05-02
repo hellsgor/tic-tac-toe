bfe8a0ed622fb46650e78e20b3accd22f91073c3
Igor Storchevoy <hellsgor@gmail.com>
2025-05-02 21:42:23 +0400

    Update[Container]: refactor Container component to support centered alignment and gap properties

--- src/UI/Container/Container.tsx
+++ src/UI/Container/Container.tsx
@@ -3,10 +3,25 @@ import { ReactNode } from "react";
 
 export function Container({
   children,
+  centered = false,
+  gap,
   className,
 }: {
   children: ReactNode;
   className?: string;
+  centered?: boolean;
+  gap?: number;
 }) {
-  return <div className={clsx("w-full", "px-8", className)}>{children}</div>;
+  return (
+    <div
+      className={clsx(
+        "flex w-full flex-col px-8",
+        centered && "items-center justify-center",
+        gap && `gap-${gap}`,
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
 }
--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -5,11 +5,9 @@ import { Container } from "@/UI/Container";
 
 export default function Game() {
   return (
-    <Container>
-      <div className="flex w-full flex-col items-center justify-center gap-10">
-        <Info />
-        <Field />
-      </div>
+    <Container centered={true} gap={10}>
+      <Info />
+      <Field />
     </Container>
   );
 }
