0f87903d99de8f388edfdc837f40671aa8ec1ff1
Igor Storchevoy <hellsgor@gmail.com>
2025-05-14 13:10:54 +0400

    Update[InputText]: added disabled InputText styles

--- src/UI/InputText/InputText.tsx
+++ src/UI/InputText/InputText.tsx
@@ -16,6 +16,8 @@ export const InputText: FC<InputTextProps> = ({
   required,
   helpText,
   errorText,
+  disabled,
+  onFocus,
   ...inputProps
 }) => {
   return (
@@ -29,6 +31,7 @@ export const InputText: FC<InputTextProps> = ({
             "mb-1 block w-[fit-content] text-xs font-medium text-gray-500",
             required &&
               `after:text-hover relative after:absolute after:top-[-4px] after:left-[100%] after:ml-0.5 after:content-['*']`,
+            disabled && "line-through",
           )}
         >
           {label}
@@ -38,9 +41,13 @@ export const InputText: FC<InputTextProps> = ({
         type={type}
         id={id}
         required={required}
+        disabled={disabled}
+        onFocus={onFocus}
         {...inputProps}
         className={clsx(
-          "bg-dark-bg hover:border-hover focus:border-hover block w-full rounded-lg border border-transparent px-5 py-3 font-mono text-sm outline-0 transition-colors disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500",
+          "bg-dark-bg hover:border-hover focus:border-hover block w-full rounded-lg border px-5 py-3 font-mono text-sm outline-0 transition-colors placeholder:text-gray-500 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:border-transparent",
+          !errorText && "border-transparent",
+          errorText && "border-red-500 hover:border-red-500",
         )}
       />
       {(helpText || errorText) && (
@@ -51,7 +58,7 @@ export const InputText: FC<InputTextProps> = ({
             errorText ? "text-red-500" : "text-gray-500",
           )}
         >
-          {errorText || helpText}
+          {errorText ?? helpText}
         </p>
       )}
     </div>
