c596000edbcacf4303d4c662b385d2fe07f3a9a1
Igor Storchevoy <hellsgor@gmail.com>
2025-05-18 20:43:57 +0400

    Chore[next.config.ts]: added static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  assetPrefix: process.env.ASSET_PREFIX,
+  basePath: process.env.BASE_PATH,
 };
 
 export default nextConfig;
